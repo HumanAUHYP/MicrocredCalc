e2e708b3d03560ad54b34b28f29385ccc45f5c64
humanplay228@gmail.com <humanplay228@gmail.com>
2021-06-25 12:03:52 +0300

    Catch FormatExeption

--- MicrocredCalc/MainWindow.xaml.cs
+++ MicrocredCalc/MainWindow.xaml.cs
@@ -28,44 +28,49 @@ namespace MicrocredCalc
 
         private void Calculate_Click(object sender, RoutedEventArgs e)
         {
-            
-            int term = int.Parse(tbTerm.Text);
-            double loanSum = double.Parse(tbSum.Text);
-
-            int firstPart = term / 3;
-            int secondPart = firstPart * 2;
-
-
-            Dictionary<int, double> daysOnTerm = new Dictionary<int, double>();
-
-            for (int i = 1; i < firstPart; i++)
+            try
             {
-                daysOnTerm.Add(i, double.Parse(tbFirstPart.Text));
-            }
-            for (int i = firstPart; i < secondPart; i++)
-            {
-                daysOnTerm.Add(i, double.Parse(tbSecondPart.Text));
-            }
-            for (int i = secondPart; i <= term; i++)
-            {
-                daysOnTerm.Add(i, double.Parse(tbThirdPart.Text));
-            }
+                int term = int.Parse(tbTerm.Text);
+                double loanSum = double.Parse(tbSum.Text);
 
-            double percentSum = 0;
-            string detail = "День\tСтавка\tДолг\tСумма выплаты";
+                int firstPart = term / 3;
+                int secondPart = firstPart * 2;
 
-            foreach (var pair in daysOnTerm)
-            {
-                Console.WriteLine($"{pair.Key} {pair.Value}");
-                percentSum += loanSum * (pair.Value / 100);
-                detail += $"\n{pair.Key}\t{pair.Value}\t{percentSum}\t{loanSum + percentSum}";
+
+                Dictionary<int, double> daysOnTerm = new Dictionary<int, double>();
+
+                for (int i = 1; i < firstPart; i++)
+                {
+                    daysOnTerm.Add(i, double.Parse(tbFirstPart.Text));
+                }
+                for (int i = firstPart; i < secondPart; i++)
+                {
+                    daysOnTerm.Add(i, double.Parse(tbSecondPart.Text));
+                }
+                for (int i = secondPart; i <= term; i++)
+                {
+                    daysOnTerm.Add(i, double.Parse(tbThirdPart.Text));
+                }
+
+                double percentSum = 0;
+                string detail = "День\tСтавка\tДолг\tСумма выплаты";
+
+                foreach (var pair in daysOnTerm)
+                {
+                    Console.WriteLine($"{pair.Key} {pair.Value}");
+                    percentSum += loanSum * (pair.Value / 100);
+                    detail += $"\n{pair.Key}\t{pair.Value}\t{percentSum}\t{loanSum + percentSum}";
+                }
+
+                lbPaymentSum.Content += Convert.ToString(loanSum + percentSum);
+                lbPercentSum.Content += Convert.ToString(percentSum);
+                lbEffRate.Content += Convert.ToString(Math.Round(percentSum / loanSum / term * 100, 2));
+                tbDetail.Text = detail;
             }
-
-            lbPaymentSum.Content += Convert.ToString(loanSum + percentSum);
-            lbPercentSum.Content += Convert.ToString(percentSum);
-            lbEffRate.Content += Convert.ToString(Math.Round(percentSum / loanSum / term * 100, 2));
-            tbDetail.Text = detail;
-            
+            catch (FormatException)
+            {
+                MessageBox.Show("Неверный формат ввода");
+            }
         }
     }
 }
